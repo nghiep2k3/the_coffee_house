cc31a8693129f956a144ca3c56d5c6b97d7e0873
VinhT3rB0 <vinhvinh132465@gmail.com>
2024-05-03 09:44:22 +0700

    form_login

--- index.php
+++ index.php
@@ -10,39 +10,46 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <header>
-        <div style="width: 60px;">
-            <a href="" class="logo">
-            <img src="./assest/img/logo.png" alt="">
-            </a>
-        </div>
-        <i class="ti-menu-alt"></i>
-        <ul class="nav-bar">
-            <li><a href="">Home</a></li>
-            <li><a href="">About us</a></li>
-            <li><a href="">Product</a></li>
-            <li><a href="">Customer</a></li>
-        </ul>
-        <div class="header-icon">
-            <i class="header-i ti-shopping-cart"></i>
-            <i class="header-i ti-search"></i>
-        </div>
-        <div class="form">
-            <form action="index1.php" method="post">
-                <button class="login" type="submit" name="switch-login">Đăng nhập</button>
-                <a href="logout.php">
-                    <button class="login" type="submit" name="switch-logout">/Đăng ký</button>
+        <header>
+            <div style="width: 60px;">
+                <a href="" class="logo">
+                <img src="./assest/img/logo.png" alt="">
                 </a>
-            </form>
+            </div>
+            <ul class="nav-bar">
+                <li><a href="">Trang chủ</a></li>
+                <li><a href="">Đặt chỗ</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Giới thiệu</a></li>
+                <li><a href="">Shop</a></li>
+
+            </ul>
+            <div class="header-icon">
+                <i class="header-i ti-shopping-cart"></i>
+                <i class="header-i ti-search"></i>
+                <i class="header-i ti-menu-alt"></i>
+            </div>
+            <div class="form">
+                <form action="index.php" method="post">
+                    <?php 
+                        include "login.php";
+                        if (isset($_SESSION['mySession'])) {
+                            echo '<div>Chào mừng, ' . $_SESSION['mySession'] . '</div>';
+                        }
+                    ?>
+                    <button class="login" type="submit" name="switch-login">Đăng nhập</button>
+                    <a href="logout.php">
+                        <button class="login" type="submit" name="switch-logout">/Đăng ký</button>
+                    </a>
+                </form>
+            </div>
+        </header>
+        <div class="main">
+            
         </div>
-    </header>
     <?php
         if(isset($_POST['switch-login'])) {
-            echo'Đăng nhập';
-            header('location:login.php');
-        }
-        if(!isset($_SESSION['mySession'])) {
-            echo '<button class="login" type="submit" name="switch-login">(' . $_SESSION['mySession'] . ')</button>';
+            header('location:form_login.php');
         }
     ?>
 </body>
--- login.php
+++ login.php
@@ -3,15 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./assest/icons/themify-icons/themify-icons.css">
     <title>Document</title>
 </head>
 <body>
     <?php
         include "connect.php";
         session_start();
-
         if(isset($_POST['login'])) {
             $username = $_POST['email'];
             $password = $_POST['password'];
@@ -26,38 +23,5 @@
             }
         }
     ?>
-<!-- <div class="modal js-modal"> -->
-    <form action="login.php" method="post">
-        <div class="modal-container js-modal-container">
-            <div class="modal-close js-modal-close">
-                <i class="ti-close"></i>
-            </div>
-            <header class="modal-header">
-                <i class="modal-heading-icon ti-bag">
-                    Order
-                </i>
-            </header>
-            <div class="modal-body">
-                <label for="send-email" class="modal-lable">
-                    <i class="ti-user"></i>
-                    Tên của bạn
-                </label>
-                <input id="send-email" name="email" type="text" required class="modal-input" placeholder="Enter your name...">
-                <label for="send-password" class="modal-lable">
-                    <i class="ti-home"></i>
-                    Mật khẩu
-                </label>
-                <input id="send-address" name="password" type="password" required class="modal-input"
-                    placeholder="Enter your password...">
-                <button class="login-js" type="submit" name="login" id="buy-ticket">
-                    Đăng Nhập <i class="ti-check"></i>
-                </button>
-            </div>
-            <footer class="modal-footer">
-                <p class="modal-help">Need <a href="#">help?</a></p>
-            </footer>
-        </div>
-    </form>
-        <!-- </div> -->
 </body>
 </html>
