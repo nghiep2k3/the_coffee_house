67291f6bbc72515452e3c83a8e350e12b8cdf276
Nguyễn Nghiệp <nghiep2k3>
2024-06-12 23:21:04 +0700

    clear

--- controller/product_detail.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "../connect.php";
-$result = mysqli_query($conn, "SELECT * FROM `products` WHERE `id` = " . $_GET['id']);
-$product = mysqli_fetch_assoc($result);
-// var_dump($product);
-// exit();
-?>
